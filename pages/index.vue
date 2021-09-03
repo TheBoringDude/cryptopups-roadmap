fe05b9a988f5829ebdb5c6e9af4748e88111557b
TheBoringDude <iamcoderx@gmail.com>
2021-09-03 17:01:29 +0800

    seo fixes

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,11 @@
 </template>
 
 <script>
+const title = 'Roadmap'
+const description =
+  "Follow World of Cryptopups's plans, goals, and solutions for the future. To the moon!"
+const image = 'https://roadmap.worldofcryptopups.cf/featured-white.png'
+
 export default {
   async asyncData({ $content }) {
     const plans = await $content('roadmap')
@@ -30,14 +35,54 @@ export default {
   },
 
   head() {
+    // https://nuxtjs.org/examples/seo-twitter-og
     return {
-      title: 'Roadmap',
+      title,
       meta: [
         {
-          hid: 'description',
-          name: 'description',
-          content:
-            "Follow World of Cryptopups's plans, goals, and solutions for the future. To the moon! ",
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: description,
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: image,
+        },
+        {
+          hid: 'twitter:image:alt',
+          name: 'twitter:image:alt',
+          content: title,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: description,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: image,
+        },
+        {
+          hid: 'og:image:secure_url',
+          property: 'og:image:secure_url',
+          content: image,
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: title,
         },
       ],
     }
