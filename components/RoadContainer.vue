16d90bed7a4bade4361d38ece062d6a186d2f0f4
TheBoringDude <iamcoderx@gmail.com>
2021-09-03 20:20:54 +0800

    add circles as denote to done in roadmap

--- components/RoadContainer.vue
+++ components/RoadContainer.vue
@@ -18,29 +18,40 @@
         bg-white
         py-8
         px-6
-        mx-4
         shadow-2xl
         rounded-xl
         border border-warmGray-200
       "
     >
-      <h2 class="font-black text-2xl tracking-wide text-gray-600">
+      <h2 class="font-black text-xl lg:text-2xl tracking-wide text-gray-600">
         {{ plan.title }}
       </h2>
       <ul class="ml-5 mt-2">
         <li
           v-for="(e, idx) in plan.events"
           :key="idx"
-          class="text-lg my-2 list-disc"
+          class="lg:text-lg my-2 list-disc"
         >
           {{ e }}
         </li>
       </ul>
     </div>
 
+    <div
+      class="h-8 w-8 border-4 border-warmGray-500 rounded-full z-50"
+      :class="plan.done ? 'bg-warmGray-500' : 'bg-white'"
+    ></div>
+
     <div class="w-full md:w-1/2">
       <h3
-        class="font-black mx-12 text-5xl tracking-wide text-warmGray-700"
+        class="
+          font-black
+          mx-0
+          md:mx-12
+          text-5xl
+          tracking-wide
+          text-warmGray-700
+        "
         :class="index % 2 == 0 ? 'md:text-left' : 'md:text-right'"
       >
         {{ plan.no }}
