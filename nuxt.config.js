10ad47c1e80a3aec5c9fa63ba44e896961e5739d
TheBoringDude <iamcoderx@gmail.com>
2021-09-03 16:49:01 +0800

    use 'ssr'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
-  mode: 'universal',
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
