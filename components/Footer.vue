6842676737631481a389088498c4b02d39b18159
TheBoringDude <iamcoderx@gmail.com>
2022-01-04 00:00:09 +0800

    Start re-write with Nuxt v3.

--- components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="py-8 text-center bg-warmGray-900 text-white tracking-wide">
-    <p>&copy; {{ new Date().getFullYear() }} | World of Cryptopups</p>
-  </footer>
-</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: [],
-  mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
+  content: ["./**/*.vue"],
   theme: {
-    colors,
-    fontFamily: {
-      sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
-    },
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
